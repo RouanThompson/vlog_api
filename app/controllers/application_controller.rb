efbe87df40fde4de900b143ee0d06c615057d2cb
Rouan Thompson <rouan.thompson@gmail.com>
2020-11-27 17:03:33 -0500

    fixed issue where show action was not displyaying users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    # def encode_token(payload)
+    #     JWT.encode(payload, 'my_s3cr3t')
+    #     # #encode will return a encoded string...this is the JWT
+    #     # encoded string contains header.payload.secret
+    # end
+
+    # def decoded_token(token)
+    #     JWT.decode(token, 'my_s3cr3t')[0]
+    #     # #decode will return an array of two elements: payload & header
+    #     # [0] will specify the first element which is the payload
+    # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,12 @@ class UsersController < ApplicationController
 
     def show
         users = User.all
-        render jsons: users
+        render json: users
     end
     
     # create controller action
     def create 
         user = User.create(user_params)
-        # byebug
         if user.valid?
             render json: { user: UserSerializer.new(user) }, status: :created
             # if the user is valid send back a filtered json to the frontend
